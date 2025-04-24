6d1da035ea084ccd9015418a483e6244e500244d
fsatsuki <fsatsuki@amazon.co.jp>
2025-04-24 08:34:07 +0000

    CIエラー解消

--- cdk/lib/constructs/ec2-gc/sfn/index.ts
+++ cdk/lib/constructs/ec2-gc/sfn/index.ts
@@ -15,7 +15,7 @@ export interface EC2GarbageCollectorStepFunctionsProps {
 export class EC2GarbageCollectorStepFunctions extends Construct {
   constructor(scope: Construct, id: string, props: EC2GarbageCollectorStepFunctionsProps) {
     super(scope, id);
-    
+
     // Set the appropriate calculation formula based on expirationInDays
     let expirationFormula: string;
     expirationFormula = `{% $millis() - 1000 * 60 * 60 * 24 * ${props.expirationInDays} %}`;
@@ -25,44 +25,48 @@ export class EC2GarbageCollectorStepFunctions extends Construct {
       assumedBy: new iam.ServicePrincipal('states.amazonaws.com'),
       description: 'Role for EC2GarbageCollector Step Functions',
     });
-    
+
     // EC2 instance-related permissions
-    stateMachineRole.addToPolicy(new iam.PolicyStatement({
-      actions: [
-        'ec2:DescribeInstances',
-        'ec2:TerminateInstances',
-        'ec2:DescribeImages',
-        'ec2:DeregisterImage',
-        'ec2:DeleteSnapshot',
-      ],
-      resources: ['*'],
-    }));
-    
+    stateMachineRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'ec2:DescribeInstances',
+          'ec2:TerminateInstances',
+          'ec2:DescribeImages',
+          'ec2:DeregisterImage',
+          'ec2:DeleteSnapshot',
+        ],
+        resources: ['*'],
+      })
+    );
+
     // SSM Parameter Store-related permissions
-    stateMachineRole.addToPolicy(new iam.PolicyStatement({
-      actions: [
-        'ssm:GetParameter',
-      ],
-      resources: ['arn:aws:ssm:*:*:parameter/remote-swe/worker/ami-id'],
-    }));
-    
+    stateMachineRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ['ssm:GetParameter'],
+        resources: ['arn:aws:ssm:*:*:parameter/remote-swe/worker/ami-id'],
+      })
+    );
+
     // ImageBuilder-related permissions
-    stateMachineRole.addToPolicy(new iam.PolicyStatement({
-      actions: [
-        'imagebuilder:DeleteImage',
-      ],
-      resources: ['*'],
-    }));
+    stateMachineRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ['imagebuilder:DeleteImage'],
+        resources: ['*'],
+      })
+    );
 
     // Path to ASL file
     const aslPath = path.join(__dirname, 'asl.json');
-    
+
     // Create state machine (using definitionSubstitutions to replace placeholders)
     const stateMachine = new sfn.StateMachine(this, 'EC2GarbageCollector', {
       definitionBody: sfn.DefinitionBody.fromString(fs.readFileSync(aslPath, 'utf8')),
       definitionSubstitutions: {
         expirationThreshold: expirationFormula,
-        imageRecipeNamePattern: props.imageRecipeName ? `${props.imageRecipeName}*` : 'RemoteSweStackSandboxWorkerImageBuilderImagePipelineV26F9C4AFCB6F87B*'
+        imageRecipeNamePattern: props.imageRecipeName
+          ? `${props.imageRecipeName}*`
+          : 'RemoteSweStackSandboxWorkerImageBuilderImagePipelineV26F9C4AFCB6F87B*',
       },
       timeout: cdk.Duration.seconds(600),
       role: stateMachineRole,
