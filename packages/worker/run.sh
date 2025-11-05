71bf2d18468442d6729b48ecd82f3fd6a1632ab3
Masashi Tomooka <mtomooka@amazon.co.jp>
2025-11-05 22:30:48 +0900

    fix: github PAT is not available on AgentCore runtime

--- packages/worker/run.sh
+++ packages/worker/run.sh
@@ -12,7 +12,7 @@ if [ -n "$GITHUB_APP_PRIVATE_KEY_PARAMETER_NAME" ]; then
 fi
 
 if [ -n "$GITHUB_PERSONAL_ACCESS_TOKEN_PARAMETER_NAME" ]; then
-    export GITHUB_PERSONAL_ACCESS_TOKEN=$(aws ssm get-parameter --name $GITHUB_PERSONAL_ACCESS_TOKEN_PARAMETER_NAME --query \"Parameter.Value\" --output text)
+    export GITHUB_PERSONAL_ACCESS_TOKEN=$(aws ssm get-parameter --name $GITHUB_PERSONAL_ACCESS_TOKEN_PARAMETER_NAME --query "Parameter.Value" --output text)
 fi
 
 if [ -n "$SLACK_BOT_TOKEN_PARAMETER_NAME" ]; then
